86361edbf5e62e3c31ca987f4fbf3634cb07332a
emilvuorio <75314539+emilvuorio@users.noreply.github.com>
2022-04-01 00:03:12 +0300

    Fixed backtracking algo

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def check_possible(y, x, n):
     for i in range(0,9):
         if board[i][x] == n:
             return False
-    
+
     x0 = (x // 3) * 3
     y0 = (y // 3) * 3
     for i in range(0,3):
@@ -32,16 +32,19 @@ def check_possible(y, x, n):
 
 def solve():
     global board
-    for y in range(0,9):
-        for x in range(0,9):
+    for y in range(9):
+        for x in range(9):
             if board[y][x] == 0:
                 for n in range(1,10):
                     if check_possible(y,x,n):
                         board[y][x] = n
-                        draw_sudoku()
                         solve()
                         board[y][x] = 0
                 return
+            
+    draw_sudoku()
+    
+        
 
 
 
@@ -57,4 +60,4 @@ def draw_sudoku():
             print("+" + "   +"*9)
 
 solve()
-draw_sudoku()
+
