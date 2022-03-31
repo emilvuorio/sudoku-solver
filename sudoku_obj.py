feccbb490a13ed34914e631e6df7647d84c038f2
Emil Vuorio <vuorio.emil@gmail.com>
2022-03-31 20:25:18 +0300

    Started on backtracking algo

--- sudoku_obj.py
+++ sudoku_obj.py
@@ -1,10 +1,14 @@
+import enum
+from re import X
+
+
 class Sudoku:
 
     def __init__(self):
 
         self.__board = self
 
-    def make(self, difficulty):
+    def make_board(self, difficulty):
 
         if difficulty == "easy":
             file = open("C:\\Koulu\OOP\\sudoku_solver\\sudokus\\easy\\easy_sudoku1.txt", "r")
@@ -12,13 +16,24 @@ class Sudoku:
             file = open("C:\\Koulu\OOP\\sudoku_solver\\sudokus\\medium\\medium_sudoku1.txt", "r")
         else:
             file = open("C:\\Koulu\OOP\\sudoku_solver\\sudokus\\hard\\hard_sudoku1.txt", "r")
-        board = []
-        line = file.read()
-        line = line.split("\n")
-        for char in line:
-            char = list(char)
-            board.append(char)
+        board = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]]
+        #board_f = file.read()
+        #board_f = board_f.split("\n") #split the rows with enter
+        #for line in board_f: #
+         #   line = list(line) #makes a line to a list
+          #  for n, i in enumerate(line): # loops through line, converts to int
+           #     line[n] = int(i)
+           # board.append(line)
         self.__board = board
+        
         
 
 
@@ -34,17 +49,42 @@ class Sudoku:
 
 
         # jotain testailua kello 23:17 yöllä
-        x0 = (3//3) * 3
-        y0 = (2//3) * 3
+
+
+    
+    def check_possible(self, y, x, n):
+        for i in range(0,9):
+            if self.__board[y][i] == n:
+                return False
+        for i in range(0,9):
+            if self.__board[i][x] == n:
+                return False
+        
+        x0 = (x // 3) * 3
+        y0 = (y // 3) * 3
         for i in range(0,3):
             for j in range(0,3):
-                print(self.__board[y0+i][x0+j])
+                if self.__board[y0+i][x0+j] == n:
+                    return False
+        return True
+        
+
+
 
     def solve(self):
-        print("Solving sudoku...")
-
+        for y in range(0,9):
+            for x in range(0,9):
+                if self.__board[y][x] == 0:
+                    for n in range(1,10):
+                        if self.check_possible(y,x,n):
+                            self.__board[y][x] = n
+                            self.draw()
+                            self.solve()
+                            self.__board[y][x] = 0
+                    return 
+        
 
 sudoku = Sudoku()
-sudoku.make("easy")
-sudoku.draw()
+sudoku.make_board("easy")
 sudoku.solve()
+sudoku.draw()
--- test.py
+++ test.py
@@ -1,5 +1,60 @@
-x = 7
-y = 4
 
-x0 = ( x // 3) * 3
-print(x0)
+
+global board
+board = [[1,0,7,6,8,3,0,4,0],
+[0,4,2,9,1,0,0,0,6],
+[0,6,8,0,0,7,0,3,0],
+[0,0,0,1,3,2,7,9,0],
+[0,0,0,0,0,8,0,2,1],
+[2,0,9,7,6,0,3,8,0],
+[4,7,3,0,0,0,0,0,8],
+[8,0,0,0,0,0,0,6,0],
+[9,0,0,0,7,0,0,0,3]]
+
+
+
+def check_possible(y, x, n):
+    global board
+    for i in range(0,9):
+        if board[y][i] == n:
+            return False
+    for i in range(0,9):
+        if board[i][x] == n:
+            return False
+    
+    x0 = (x // 3) * 3
+    y0 = (y // 3) * 3
+    for i in range(0,3):
+        for j in range(0,3):
+            if board[y0+i][x0+j] == n:
+                return False
+    return True
+
+def solve():
+    global board
+    for y in range(0,9):
+        for x in range(0,9):
+            if board[y][x] == 0:
+                for n in range(1,10):
+                    if check_possible(y,x,n):
+                        board[y][x] = n
+                        draw_sudoku()
+                        solve()
+                        board[y][x] = 0
+                return
+
+
+
+
+def draw_sudoku():
+    global board
+    print("+" + "---+"*9)
+    for i, row in enumerate(board):
+        print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
+        if i % 3 == 2:
+            print("+" + "---+"*9)
+        else:
+            print("+" + "   +"*9)
+
+solve()
+draw_sudoku()
