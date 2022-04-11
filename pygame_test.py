e40a1adc3f88d8a375e5a02d82a6f90971f47920
Your Name <you@example.com>
2022-04-11 17:10:52 +0300

    Printed sudoku board and added user input in pygame

--- pygame_test.py
+++ pygame_test.py
@@ -1,11 +1,14 @@
+from distutils.log import set_threshold
 import pygame
 import sys
 
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 700))
 icon = pygame.image.load("sudoku_icon.png")
 pygame.display.set_icon(icon)
+background_color = (0, 0, 0)
+
 
 sudoku = [[5,3,0,0,7,0,0,0,0],
         [6,0,0,1,9,5,0,0,0],
@@ -16,29 +19,68 @@ sudoku = [[5,3,0,0,7,0,0,0,0],
         [0,6,0,0,0,0,2,8,0],
         [0,0,0,4,1,9,0,0,5],
         [0,0,0,0,8,0,0,7,9]]
+    
 
-def draw_sudoku(sudoku):
-        print("+" + "---+"*9)
-        for i, row in enumerate(sudoku):
-            print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
-            if i % 3 == 2:
-                print("+" + "---+"*9)
-            else:
-                print("+" + "   +"*9)
+font1 = pygame.font.SysFont("Comic Sans MS", 35)
+font2 = pygame.font.SysFont("Comic Sans MS", 20)
 
-#Sudoku = pygame.draw_sudoku(sudoku)
-#sudokuX = 200
-#sudokuY = 200
+row_text=""
+grid_text=""
+numberext=""
+row_input = pygame.Rect(50, 50, 100, 32)
+grid_input = pygame.Rect(100,50,50,32)
+number_input=pygame.Rect(150,70,50, 32)
 
-#def soudouku():
- #   screen.blit(Sudoku, (sudokuX, sudokuY))
+screen.fill((153, 204, 255))
+
+
+color_active = pygame.Color('green')
+color_passive = pygame.Color('green')
+color = color_passive
+
+for i in range(0,10):
+    if (i%3 == 0  ):
+        pygame.draw.line(screen, (0,0,0), (50 + 50*i,50),(50 + 50*i, 500),4)
+        pygame.draw.line(screen, (0,0,0), (50, 50 + 50*i),(500, 50 + 50*i),4)
+    pygame.draw.line(screen, (0,0,0), (50 + 50*i ,50),(50 + 50*i, 500),2)
+    pygame.draw.line(screen, (0,0,0), (50, 50 + 50*i),(500, 50 + 50*i),2)
+pygame.display.update()
+
+for i in range (0, len(sudoku[0])):
+    for j in range (0, len(sudoku[0])):
+        if (0<sudoku[i][j]<10):
+            value = font1.render(str(sudoku[i][j]), True, background_color)
+            screen.blit(value, ((j+1)*50 +15, (i+1)*50))
+pygame.display.update()
+
+active = False
 
 while True:
-    screen.fill((153, 204, 255))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if row_input.collidepoint(event.pos):
+                active == True
+            else:
+                active = False
+        if event.type == pygame.KEYDOWN:
 
-    #soudouku()
+            if event.key == pygame.K_BACKSPACE:
+                user_text = user_text[:9]
+
+            else:
+                row_text += event.unicode
+
+    if active:
+        color = color_active
+    else:
+        color = color_passive
+
+    pygame.draw.rect(screen, color, row_input)
+    user_text = font2.render(row_text, True, (255, 255, 255))
+    screen.blit(user_text, (row_input.x+5, row_input.y+5))
+    row_input.w = max(100, user_text.get_width()+10)
+    pygame.display.flip()
     pygame.display.update()
-    draw_sudoku(sudoku)
